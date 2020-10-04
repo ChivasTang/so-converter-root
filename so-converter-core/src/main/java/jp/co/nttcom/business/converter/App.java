951fb0fdcd2aec97c71c1accf7cee3f75cb29bb2
tangzh1983@gmail.com <tangzh1983@gmail.com>
2020-10-04 18:18:26 +0900

    20201004

---
+++ so-converter-core/src/main/java/jp/co/nttcom/business/converter/App.java
@@ -0,0 +1,13 @@
+package jp.co.nttcom.business.converter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
